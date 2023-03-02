a938bbe554eda9d08ab224cd8f64aedfff09e748
mhernangilp <mhernangilp@gmail.com>
2023-03-02 03:50:11 +0100

    created function that downloads target data

--- bot.py
+++ bot.py
@@ -1,5 +1,55 @@
 import subprocess
 
-o = subprocess.run('ls', shell=True, capture_output=True)
-sol = o.stdout.decode()
-print(sol)
+# function to load usr and pwd from file
+'''def getCredentials():
+    print("Where is allocated your credentials.txt: ")
+    response = str(input().strip())
+    response = "../../yehe.txt"                  # << delete in future
+    with open(response) as fp:
+        line = fp.readline()
+        usr = str(line.strip())
+        line = fp.readline()
+        pwd = str(line.strip())
+    return usr, pwd
+'''
+
+def downloadData():
+    print("Select target account:")
+    targetUsr = str(input().strip())
+    targetUsr = "luisglez._"                   # << delete in future
+    print("Downloading please wait...\n")
+    subprocess.run(["instaloader", "--fast-update", targetUsr], capture_output=True)
+    print("---Data downloaded successfully---")
+    return targetUsr
+
+
+def downloadDataLogin():
+    print("Where is your downloadUser.txt:")
+    response = str(input().strip())
+    response = "../../yehe.txt"                  # << delete in future
+    print("Select target account:")
+    targetUsr = str(input().strip())
+    targetUsr = "luisglez._"                   # << delete in future
+    print("Downloading please wait...\n")
+    with open(response) as fp:
+        line = fp.readline()
+        usr = str(line.strip())
+    subprocess.run(["instaloader", "--login=" + usr, "--fast-update", targetUsr], capture_output=True)
+    print("---Data downloaded successfully---")
+    return usr, targetUsr
+
+
+def instaloader():
+    print("Choose an option for the download of target's pictures:")
+    print("1.- Without login");
+    print("2.- With login (needed if target's account is private and better photo quality)")
+    resp = int(input().strip())
+    if resp == 1:
+        return "", downloadData()
+    else:
+        return downloadDataLogin()
+
+
+usrLoginDir, targetUsr = instaloader()
+print(usrLoginDir)
+print(targetUsr)
