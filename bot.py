74b6485385b58a6f9c4f8ac413ea6d4a69ac204b
mhernangilp <mhernangilp@gmail.com>
2023-03-03 01:23:23 +0100

    added download stories and catching exceptions

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 import time
 
 # function to load usr and pwd from file
@@ -14,14 +15,21 @@ import time
     return usr, pwd
 '''
 
-def downloadData(targetUsr):
+def downloadData(targetUsr, mode):
     print("Downloading please wait...\n")
-    subprocess.run(["instaloader", "--fast-update", targetUsr], capture_output=True)
+    shell = ["instaloader", "--fast-update", targetUsr]
+    if mode == 1:
+        shell.insert(1, "--stories")
+    try:
+        subprocess.run(shell, capture_output=True)
+    except Exception as e:
+        print(e)
+        sys.exit()
     print("---Data downloaded successfully---")
     return ""
 
 
-def downloadDataLogin(targetUsr):
+def downloadDataLogin(targetUsr, mode):
     print("Enter username:")
     response = str(input().strip())
     response = "../../yehe.txt"                  # << delete in future
@@ -29,25 +37,32 @@ def downloadDataLogin(targetUsr):
     with open(response) as fp:                 # << ''
         line = fp.readline()                   # << ''
         usr = str(line.strip())                # << ''
-    subprocess.run(["instaloader", "--login=" + usr, "--fast-update", targetUsr], capture_output=True)
+    shell = ["instaloader", "--login=" + usr, "--fast-update", targetUsr]
+    if mode == 1:
+        shell.insert(1, "--stories")
+    try:
+        subprocess.run(shell, capture_output=True)
+    except Exception as e:
+        print(e)
+        sys.exit()
     print("---Data downloaded successfully---")
     return usr
 
 
-def instaloader(targetUsr):
+def instaloader(targetUsr, mode):
     print("Choose an option for the download of target's pictures:")
-    print("1.- Without login");
+    print("1.- Without login")
     print("2.- With login (needed if target's account is private and better photo quality)")
     resp = int(input().strip())
     if resp == 1:
-        return downloadData(targetUsr)
+        return downloadData(targetUsr, mode)
     else:
-        return downloadDataLogin(targetUsr)
+        return downloadDataLogin(targetUsr, mode)
 
-def loadData(targetUsr):
+def loadPosts(targetUsr):
     ret = {}
     ls = subprocess.run(["ls", targetUsr], capture_output=True)
-    grep = subprocess.run(["grep", ".jpg"], capture_output=True, input=ls.stdout)
+    grep = subprocess.run(["grep", "-e", ".jpg", "-e", ".mp4"], capture_output=True, input=ls.stdout)
     jpg = grep.stdout.decode().split()
     for photo in jpg:
         if "profile_pic" in photo:
@@ -68,17 +83,44 @@ def loadData(targetUsr):
     return ret
 
 
+def loadStories(targetUsr):
+    ret = []
+    ls = subprocess.run(["ls", targetUsr], capture_output=True)
+    grep = subprocess.run(["grep", "-e", ".jpg", "-e", ".mp4"], capture_output=True, input=ls.stdout)
+    jpg = grep.stdout.decode().split()
+    for photo in jpg:
+        if "profile_pic" not in photo:
+            ret.append(photo)
+    return ret
+
 def updateDataFirstCall():
     print("Select target account:")
     targetUsr = str(input().strip())
     targetUsr = "luisglez._"                   # << delete in future
-    firstRun = loadData(targetUsr)
-    loginUsr = instaloader(targetUsr)
-    secndRun = loadData(targetUsr)
-    updatedData = secndRun.copy()
+    firstRun = loadPosts(targetUsr)
+    print("\n.:Download posts:.\n")
+    loginUsr = instaloader(targetUsr, 0)
+    secndRun = loadPosts(targetUsr)
+    updatedData = [secndRun.copy(), []]
     for date in secndRun:
         if date in firstRun:
-            del updatedData[date]
+            del updatedData[0][date]
+    firstRun = loadStories(targetUsr)
+    print("\n.:Download stories.:\n")
+    loginUsr = instaloader(targetUsr, 1)
+    secndRun = loadStories(targetUsr)
+    updatedData[1] = secndRun.copy()
+    for date in secndRun:
+        if date in firstRun:
+            updatedData[1].remove(date)
+    for jpg in updatedData[1]:
+        if ".jpg" in jpg:
+            date1 = jpg.replace(".jpg", "")
+            for mp4 in updatedData[1]:
+                if ".mp4" in mp4:
+                    date2 = mp4.replace(".mp4", "")
+                    if date1 == date2:
+                        updatedData[1].remove(jpg)
     return updatedData, loginUsr, targetUsr
 
 
